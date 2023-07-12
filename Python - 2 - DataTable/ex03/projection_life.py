2bacf9b14457e6db35d98ae3920f1a3fa7dda5b6
Noe Dormoy <noedormoy@macbook-pro-de-noe-2.home>
2023-07-12 22:45:56 +0200

    Ex03 OK log works, day02 OK, try to use more seaborn

--- Python - 2 - DataTable/ex03/projection_life.py
+++ Python - 2 - DataTable/ex03/projection_life.py
@@ -2,18 +2,8 @@ import seaborn as sns
 from load_csv import load
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
-
-# def convert_number(nb):
-#     """Convert nb  to real numbers"""
-#     suffix = nb[-1]
-#     value = float(nb[:-1])
-#     if suffix == 'M':
-#         return value * 1e6
-#     elif suffix == 'k':
-#         return value * 1e3
-#     else:
-#         return value
 
 
 def main():
@@ -29,15 +19,20 @@ def main():
         merged_df = income_df.merge(life_expect_df, on="country", how="outer")
         merged_df = merged_df.rename(columns={"1900_x": "Income", "1900_y": "life_expect"})
         print(merged_df)
-        sns.scatterplot(x="Income", y="life_expect", data=merged_df)
+        g = sns.scatterplot(x="Income", y="life_expect", data=merged_df)
         plt.xlabel("Gross domestic product")
         plt.ylabel("Life Expectancy")
         plt.title("1900")
-        # plt.xticks([0, 20_000_000, 40_000_000, 60_000_000],
-        #         ["", "20M", "40M", "60M"])
+        
+        # sns.plot(x_scale='log')
+        # p1.add(so.Dots()).scale(y="log")
+        plt.xscale('log')
+        x_locator = ticker.FixedLocator([300, 1000, 10000])
+        x_formatter = ticker.FixedFormatter(['300', '1k', '10k'])
+        # Apply the custom locator and formatter to the x-axis
+        plt.gca().xaxis.set_major_locator(x_locator)
+        plt.gca().xaxis.set_major_formatter(x_formatter)
         plt.show()
-
-
 
 
     except AssertionError as e:
