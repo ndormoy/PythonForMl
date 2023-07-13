1eb72f4c7901b21296df38ed40311704a70213f6
Noe Dormoy <noedormoy@macbook-pro-de-noe-2.home>
2023-07-13 15:34:04 +0200

    Day03 ex03 OK

---
+++ Python - 3 - OOP/ex03/ft_calculator.py
@@ -0,0 +1,20 @@
+class calculator:
+
+    def __init__(self, vector):
+        self.vector = vector
+
+    def __add__(self, scalar) -> None:
+        self.vector = [x + scalar for x in self.vector]
+        print(self.vector)
+
+    def __mul__(self, scalar) -> None:
+        self.vector = [x * scalar for x in self.vector]
+        print(self.vector)
+
+    def __sub__(self, scalar) -> None:
+        self.vector = [x - scalar for x in self.vector]
+        print(self.vector)
+
+    def __truediv__(self, scalar) -> None:
+        self.vector = [x / scalar for x in self.vector]
+        print(self.vector)
